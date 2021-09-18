d08399a9c516af9f4d433821854b169765c46180
Chawanrat Nurat <ibaart01@gmail.com>
2021-09-19 04:01:46 +0700

    React-bootstrap setup, test

--- frontend/react-part/src/App.js
+++ frontend/react-part/src/App.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom"; // don't forget to npm i react-router-dom
 import Header from "./components/Header";
+import Navbar_bootstrap from "./components/Navbar_bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="container">
-        <Header/>
+    <div>
+      <Navbar_bootstrap />
+      <div className="toDoListBox">
+        <Header />
+      </div>
     </div>
   );
 }
--- frontend/react-part/src/components/Header.js
+++ frontend/react-part/src/components/Header.js
@@ -1,8 +1,9 @@
-
+import { Button } from 'react-bootstrap'
 const Header = () => {
     return (
         <div>
             <h1>FAPP To-do list</h1>
+            <Button> ART</Button>
         </div>
     )
 }
